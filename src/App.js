eda975fc6696f058924d327215d0fb85ba323165
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2019-01-03 09:03:51 +0800

    bug fix

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ class App extends Component {
       <>
         {
           this.state.isSoundAlert && <Sound
-            url="pop.mp3"
+            url={__DEV__ ? "pop.mp3" : "/public/html/leaderboard/pop.mp3"}
             autoLoad
             ignoreMobileRestrictions
             playStatus={Sound.status.PLAYING}
