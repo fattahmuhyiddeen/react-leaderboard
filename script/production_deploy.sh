167a75f0a251273baa38cfbbe3008fddb14905f0
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2019-01-14 13:27:00 +0800

    bug fix

---
+++ script/production_deploy.sh
@@ -0,0 +1,1 @@
+npm run build &&  scp -r build ubuntu@54.169.122.165:~/leaderboard && ssh ubuntu@54.169.122.165 
---
+++ script/staging_deploy.sh
@@ -0,0 +1,1 @@
+npm run build &&  scp -r build ubuntu@13.229.99.122:~/leaderboard && ssh ubuntu@13.229.99.122 
